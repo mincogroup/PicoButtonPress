ea49cc095b51ea3010aee47f76f1c11d0d879a64
Reed Dahle <Rexius923@gmail.com>
2024-03-21 17:04:33 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,31 @@
+import board
+import digitalio
+import time
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+
+
+keyboard = Keyboard(usb_hid.devices)
+
+
+button_pin = board.GP8       # pin to connect button to
+led_pin = board.GP6   # pin to connect LED to
+
+# Initializing LED
+led = digitalio.DigitalInOut(led_pin)
+led.direction = digitalio.Direction.OUTPUT
+
+# Initializing Button
+button = digitalio.DigitalInOut(button_pin)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.DOWN
+
+
+while True:
+    if button.value:
+        keyboard.send(Keycode.F13)
+        led.value = True
+        time.sleep(1)
+        led.value = False
+    
